a54eea30283ac8a7e084caed5fb905b03f9177b0
TLRKiliann <philogenie@protonmail.com>
2026-04-01 19:10:46 +0200

    folder created

---
+++ my-app/app/ui/menu/Menu.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Menu() {
+    return (
+        <nav className="bg-slate-600/80 dark:bg-slate-700/80">
+            <ul className="list-none flex flex-row items-center justify-evenly text-base text-slate-100 font-bold py-4">
+                <li>
+                    <Link href="/fonctions-executives">Fonctions Executives</Link>
+                </li>
+                <li>
+                    <Link href="/memo-visio-space">Mémoire et Attention Visio Spaciale</Link>
+                </li>
+                <li>
+                    <Link href="/memo-verbale">Mémoire Verbale</Link>
+                </li>
+                <li>
+                    <Link href="/memo-travail">Mémoire de Travail</Link>
+                </li>
+                <li>
+                    <Link href="/vitesse-traitement">Vitesse de Traitement de l'Info</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
---
+++ my-app/app/ui/menu/OverMenu.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import DarkModeToggle from "../DarkModeToggle";
+import { MdHome } from "react-icons/md";
+
+export default function OverMenu() {
+    return (
+        <nav className="bg-slate-600 dark:bg-slate-700/70">
+            <ul className="list-none flex flex-row items-center justify-evenly text-base text-slate-100 font-bold">
+                <li className="my-4">
+                    <Link href="/">
+                        <span><MdHome size={24}/></span>
+                    </Link>
+                </li>
+                <li className="my-4">
+                    <Link href="">
+                        Patient
+                    </Link>
+                </li>
+                <li className="my-4">
+                    <Link href="">
+                        Score
+                    </Link>
+                </li>
+                <li className="my-4">
+                    <Link href="">
+                        Login
+                    </Link>
+                </li>
+                <li className="my-4">
+                    <Link href="">
+                        English
+                    </Link>
+                </li>
+                <li className="my-4">
+                    <DarkModeToggle />
+                </li>
+            </ul>
+        </nav>
+    )
+}
