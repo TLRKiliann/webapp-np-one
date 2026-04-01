458281352fa1c91b6638f8b7b98f9cde62cfbc97
TLRKiliann <philogenie@protonmail.com>
2026-04-01 18:25:44 +0200

    created

---
+++ my-app/app/attention-divisee/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function AttentionDiviseePage() {
+    return (
+        <div className="h-screen p-4">
+
+            <h1 className="text-3xl font-bold">Attention divisée</h1>
+
+            {/* <nav className="mt-8 ml-8">
+                <ul className="list-disc">
+                    <li className="text-xl hover:text-green-500 mb-2">
+                        <Link href="/language/fluence/1">
+                            Fluence sémantique: Execrice 1
+                        </Link>
+                    </li>
+                    <li className="text-xl hover:text-green-500 mb-2">
+                        <Link href="/language/fluence/2">
+                            Fluence littérale: Execrice 2
+                        </Link>
+                    </li>
+                    <li className="text-xl hover:text-green-500 mb-2">
+                        <Link href="/language/fluence/3">
+                            Associations sémantiques: Execrice 3
+                        </Link>
+                    </li>
+                </ul>
+            </nav> */}
+
+        </div>
+    )
+}
