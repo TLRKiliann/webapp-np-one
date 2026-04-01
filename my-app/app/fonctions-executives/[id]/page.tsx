b175f8fa3ba32b14d9bdd87c4a89f517d0422a52
TLRKiliann <philogenie@protonmail.com>
2026-04-01 16:42:54 +0200

    code review

--- my-app/app/fonctions-executives/[id]/page.tsx
+++ my-app/app/fonctions-executives/[id]/page.tsx
@@ -2,47 +2,38 @@ import SimpleHanoiFexec from "@/app/ui/fexec/SimpleHanoiFexec";
 import TrailMakingBFexec from "@/app/ui/fexec/TrailMakingBFexec";
 import TriCartesFexec from "@/app/ui/fexec/TriCartesFexec";
 
+const tasks = {
+    "1": {
+        title: "Trail Making B",
+        description: "Alterner chiffres et lettres (1-A-2-B...)",
+        component: <TrailMakingBFexec />
+    },
+    "2": {
+        title: "Tours de Hanoï simplifiée",
+        description: "Déplacer des disques selon des règles",
+        component: <SimpleHanoiFexec />
+    },
+    "3": {
+        title: "Tri de cartes (WCST adapté)",
+        description: "Deviner la règle de tri qui change",
+        component: <TriCartesFexec />
+    }
+};
+
 export default async function Executives({ params }: { params: Promise<{ id: string }> }) {
-    
+
     const { id } = await params;
-
-    const tasks = {
-        "1": {
-            title: "Trail Making B",
-            description: "Alterner chiffres et lettres (1-A-2-B...)"
-        },
-        "2": {
-            title: "Tours de Hanoï simplifiée",
-            description: "Déplacer des disques selon des règles"
-        },
-        "3": {
-            title: "Tri de cartes (WCST adapté)",
-            description: "Deviner la règle de tri qui change"
-        }
-    } as const;
-
     const task = tasks[id as keyof typeof tasks];
 
     if (!task) {
         return <div>Tâche non trouvée !</div>
-    }
+    };
 
     return (
         <div>
-            <h1 className="text-cyan-500">{task.title}</h1>
-
+            <h1 className="text-3xl font-bold">{task.title}</h1>
             <p>{task.description}</p>
-
-            <div>
-                {id === "1" ? ( 
-                    <TrailMakingBFexec />
-                ) : id === "2" ? (
-                    <SimpleHanoiFexec />
-                ) : (
-                    <TriCartesFexec />
-                )}
-
-            </div>
+            <div>{task.component}</div>
         </div>
     );
-}
+};
