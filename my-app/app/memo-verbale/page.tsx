b1f5df247ced857be191574cac4ed28e5fb3b8fa
TLRKiliann <philogenie@protonmail.com>
2026-04-01 20:36:20 +0200

    some changes

--- my-app/app/memo-verbale/page.tsx
+++ my-app/app/memo-verbale/page.tsx
@@ -1,5 +1,31 @@
+import Link from "next/link";
+
 export default function MemoVerbalePage() {
     return (
-        <h1>Mémoire Verbale</h1>
+        <div className="h-screen p-4">
+
+            <h1 className="text-3xl font-bold">Mémoire verbale</h1>
+
+            {/* <nav className="mt-8 ml-8">
+                <ul className="list-disc">
+                    <li className="text-xl hover:text-green-500 mb-2">
+                        <Link href="/language/fluence/1">
+                            Fluence sémantique: Execrice 1
+                        </Link>
+                    </li>
+                    <li className="text-xl hover:text-green-500 mb-2">
+                        <Link href="/language/fluence/2">
+                            Fluence littérale: Execrice 2
+                        </Link>
+                    </li>
+                    <li className="text-xl hover:text-green-500 mb-2">
+                        <Link href="/language/fluence/3">
+                            Associations sémantiques: Execrice 3
+                        </Link>
+                    </li>
+                </ul>
+            </nav> */}
+
+        </div>
     )
 }
