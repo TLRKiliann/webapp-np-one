91249f40e1feba4bd03be8e9dc443e7ea8df4cf8
TLRKiliann <philogenie@protonmail.com>
2026-04-02 10:53:20 +0200

    code completed

--- my-app/app/language/denomination/[id]/page.tsx
+++ my-app/app/language/denomination/[id]/page.tsx
@@ -1,21 +1,30 @@
+import DenoAmorcage from "@/app/ui/language-exo/denominationExo/DenoAmorcage";
+import DenoComplement from "@/app/ui/language-exo/denominationExo/DenoComplement";
+import DenoConfroVisu from "@/app/ui/language-exo/denominationExo/DenoConfroVisu";
+import DenoSurDefinition from "@/app/ui/language-exo/denominationExo/DenoSurDefinition";
+
 const tasks = {
     "1": {
         title: "Dénomination sur confrontation visuelle",
         description: "Nommer un objet affiché (image → mot)",
+        component: <DenoConfroVisu />
     },
     "2": {
-        title: "Dönomination sur döfinition",
+        title: "Dénomination sur définition",
         description: "Nommer à partir d'une description (définition → mot)",
+        component: <DenoSurDefinition />
 
     },
     "3": {
         title: "Complètement de phrase",
         description: "Compléter une phrase à trou avec l'image comme indice ('on coupe le pain avec un ...')",
+        component: <DenoComplement />
 
     },
     "4": {
-        title: "Dönomination avec amorçage",
+        title: "Dénomination avec amorçage",
         description: "Dénomination avec amorçage phonologique (première syllabe donnée en indice)",
+        component: <DenoAmorcage />
 
     }
 } as const;
@@ -33,7 +42,7 @@ export default async function DenominationExo({params}: {params: Promise<{ id: s
             <p className="mt-4 mb-6 ml-4">{task.description}</p>
 
             <div className="w-auto h-[70vh] border mx-4">
-                ...
+                {task.component}
             </div>
 
         </div>
